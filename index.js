f022234d798f64716bcd8498d99bb610029d837b
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-01-13 16:59:17 +0100

    added the winner or looser

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const img1 = document.querySelector(".img1");
 img1.src = `./images/dice${randomNumber1}.png`;
 const img2 = document.querySelector(".img2");
 img2.src = `./images/dice${randomNumber2}.png`;
+const h1 = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2) {
+    h1.innerHTML = "⛳️ Player 1 Wins";
+} else if(randomNumber1 < randomNumber2){
+    h1.innerHTML = "Player 2 Wins ⛳️";
+} else if(randomNumber1 === randomNumber2){
+    h1.innerHTML = "Draw!";
+}
